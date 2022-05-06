a0b9ee06e7686d572dfd0a80157aeefb998f0423
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-05-06 13:02:42 -0300

    Alteracoes para adicionar projeto a um repositorio pessoal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Table from './components/Table';
 import StarWarsProvider from './context/StarWarsProvider';
